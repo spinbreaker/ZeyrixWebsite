45cdd133ecf878241b17537475b6bb81e53ed9c5
xZeyrix <nikolas.chelyuk@gmail.com>
2026-08-29 22:29:53 +0500

    build: install zustand to create the separate drafts for each chat & change the routing prefix rule

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,5 +3,5 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "ru", "kk"],
   defaultLocale: "en",
-  localePrefix: "always",
+  localePrefix: "as-needed",
 });
